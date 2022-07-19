3bd61ea275d91f7a781ea0762228513115167178
SefikaOzlem <61476094+SefikaOzlem@users.noreply.github.com>
2022-07-19 22:38:54 +0300

    Activity and subclasses

--- Java_Bootcamp_Project/src/app/app.module.ts
+++ Java_Bootcamp_Project/src/app/app.module.ts
@@ -2,28 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ActitiviyComponent } from './model/actitiviy/actitiviy.component';
-import { BootcampComponent } from './model/bootcamp/bootcamp.component';
-import { HackhatonComponent } from './model/hackhaton/hackhaton.component';
-import { InHouseEducationComponent } from './model/in-house-education/in-house-education.component';
-import { PersonComponent } from './model/person/person.component';
-import { StudentComponent } from './model/student/student.component';
-import { TeacherComponent } from './model/teacher/teacher.component';
-import { ModeratorComponent } from './model/moderator/moderator.component';
-import { MentorComponent } from './model/mentor/mentor.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ActitiviyComponent,
-    BootcampComponent,
-    HackhatonComponent,
-    InHouseEducationComponent,
-    PersonComponent,
-    StudentComponent,
-    TeacherComponent,
-    ModeratorComponent,
-    MentorComponent
+    AppComponent
+  
+   
   ],
   imports: [
     BrowserModule
