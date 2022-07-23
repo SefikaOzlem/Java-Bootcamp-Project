dd91ff7da4dd1a549fa3881d225eec651d40f4b5
SefikaOzlem <61476094+SefikaOzlem@users.noreply.github.com>
2022-07-23 22:47:40 +0300

    Students List

--- Java_Bootcamp_Project/src/app/app.module.ts
+++ Java_Bootcamp_Project/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { TeacherComponent } from './components/teacher/teacher.component';
 import { ActivityComponent } from './components/activity/activity.component';
 import { ListactivitiesComponent } from './components/listactivities/listactivities.component';
-<<<<<<< HEAD
 import { StudentsComponent } from './components/students/students.component';
-
-	
-
-=======
 import { AppRoutingModule } from './app-routing/app-routing.module';
->>>>>>> f1be6ceed6e2de38d15fe63a41d972d01d7e6668
+
 
 
 @NgModule({
@@ -25,13 +18,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     ActivityComponent,
     UsersComponent,
     TeacherComponent,
-<<<<<<< HEAD
     ListactivitiesComponent,
     StudentsComponent,
-=======
     ListactivitiesComponent
->>>>>>> f1be6ceed6e2de38d15fe63a41d972d01d7e6668
-   
+
   ],
   imports: [
     BrowserModule,
